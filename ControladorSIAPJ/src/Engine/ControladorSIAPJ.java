8310db68888b3d0b88281abc3b812a574398e44b
aluno <aluno>
2017-04-07 10:17:10 -0300

    Merge issues fix

--- ControladorSIAPJ/src/Engine/ControladorSIAPJ.java
+++ ControladorSIAPJ/src/Engine/ControladorSIAPJ.java
@@ -1,8 +1,14 @@
 package Engine;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 import Interfaces.*;
 
 public class ControladorSIAPJ implements RepositorioProcessos, ServicoEmail, ValidadorProcesso{
+	
+	private Map<Integer, Processo> processos = new HashMap<Integer, Processo>();
+	
 	public boolean initProcesso(Processo p)
 	{
 		boolean ok = checkProcesso(p);
@@ -13,6 +19,7 @@ public class ControladorSIAPJ implements RepositorioProcessos, ServicoEmail, Val
 		}
 		else
 			sendInfoByEmail(p, ok);
+		return ok;
 	}
 	
 	private boolean checkProcesso(Processo p)
@@ -22,18 +29,45 @@ public class ControladorSIAPJ implements RepositorioProcessos, ServicoEmail, Val
 	
 	private Processo persistProcesso(Processo p)
 	{
-		FileWriter out = new FileWriter("database/processos.txt", true); 
-		BufferedWriter bw = new BufferedWriter(out);
+		try {
+			FileWriter out = new FileWriter("database/processos.txt", true); 
+			BufferedWriter bw = new BufferedWriter(out);
 		
-		bw.write("-------------------------------------------------------------------------------------------------------");
-		bw.write("Processo " + p.getId() + "\n");
-		bw.write("Reclamante: " + p.getReclamante() + "\nTelefone: " + p.getTelefone() + "\nE-mail: " + p.getEmail() + "\n");
-		bw.write("Conteúdo:\n" + p.getConteudo() + "\n");
+			bw.write("-------------------------------------------------------------------------------------------------------");
+			bw.write("Processo " + p.getID() + "\n");
+			bw.write("Reclamante: " + p.getNomeReclamante() + "\nTelefone: " + p.getTelefone() + "\nE-mail: " + p.getEmail() + "\n");
+			bw.write("Conteúdo:\n" + p.getContent() + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void sendInfoByEmail(Processo p, boolean statusProcesso)
 	{
 		
+	}
+
+	
+	public boolean validateProcess(Processo proc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean sendEmail(String address) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean addProcesso(Processo proc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public Processo getProcesso(int id) {
+		return this.processos.get(id);
 	}
 	
 	
