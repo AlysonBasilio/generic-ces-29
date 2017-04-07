2146491964570898f87e6cc9bacc7e64da2b1c1a
aluno <aluno>
2017-04-07 11:05:24 -0300

    Mock ServicoCarta

---
+++ ControladorSIAPJ/src/Interfaces/ServicoCartas.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface ServicoCartas {
+	public void sendCarta (String conteudo);
+}
---
+++ ControladorSIAPJ/src/Mocks/MockServicoCarta.java
@@ -0,0 +1,11 @@
+package Mocks;
+
+import Interfaces.ServicoCartas;
+
+public class MockServicoCarta implements ServicoCartas{
+	@Override
+	public void sendCarta(String conteudo) {
+		// TODO Auto-generated method stub
+		
+	}
+}
