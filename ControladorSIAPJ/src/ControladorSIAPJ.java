580f089583cb5968ccdfb209999b7ce020447ec1
aluno <aluno>
2017-04-07 10:10:58 -0300

    Desenvolvendo email

--- ControladorSIAPJ/src/ControladorSIAPJ.java
+++ ControladorSIAPJ/src/ControladorSIAPJ.java
@@ -1,6 +1,27 @@
 import java.io.*;
+import java.util.Properties;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-public class ControladorSIAPJ {
+public class ControladorSIAPJ implements ServicoEmail, RepositorioProcessos, ValidadorProcesso {
+	private Properties props;
+	
+	public void init () {
+		 props = new Properties();
+         /** Parâmetros de conexão com servidor Gmail */
+         props.put("mail.smtp.host", "smtp.gmail.com");
+         props.put("mail.smtp.socketFactory.port", "465");
+         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+         props.put("mail.smtp.auth", "true");
+         props.put("mail.smtp.port", "465");
+	}
+	
 	public boolean initProcesso(Processo p)
 	{
 		boolean ok = checkProcesso(p);
