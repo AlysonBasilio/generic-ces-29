d0385020be864d18e4908b60874d11af8566883e
aluno <aluno>
2017-04-07 10:36:00 -0300

    Send email

--- ControladorSIAPJ/src/Engine/ControladorSIAPJ.java
+++ ControladorSIAPJ/src/Engine/ControladorSIAPJ.java
@@ -19,7 +19,9 @@ import javax.mail.internet.MimeMessage;
 public class ControladorSIAPJ implements ServicoEmail, RepositorioProcessos, ValidadorProcesso {
 
 	private Properties props;
+	private Session session;
 	private Map<Integer, Processo> processos = new HashMap<Integer, Processo>();
+	String meuEmail = "xupadeputado@gmail.com";
 	
 	public void init () {
 		 props = new Properties();
@@ -31,13 +33,15 @@ public class ControladorSIAPJ implements ServicoEmail, RepositorioProcessos, Val
          props.put("mail.smtp.port", "465");
          
 
- 		Session session = Session.getDefaultInstance(props,
+ 		session = Session.getDefaultInstance(props,
                  new javax.mail.Authenticator() {
                       protected PasswordAuthentication getPasswordAuthentication() 
                       {
-                            return new PasswordAuthentication("mateuscoelho2009@gmail.com", "ronaldo");
+                            return new PasswordAuthentication(meuEmail, "xupadeputado1");
                       }
                  });
+ 		
+ 		session.setDebug(true);
 	}
 	
 	public boolean initProcesso(Processo p)
@@ -78,7 +82,7 @@ public class ControladorSIAPJ implements ServicoEmail, RepositorioProcessos, Val
 		
 	}
 
-	@Overrride
+	@Override
 	public boolean validateProcess(Processo proc) {
 		// TODO Auto-generated method stub
 		return false;
@@ -86,8 +90,23 @@ public class ControladorSIAPJ implements ServicoEmail, RepositorioProcessos, Val
 
 	@Override
 	public boolean sendEmail(String address) {
+		try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(meuEmail)); //Remetente
+
+            Address[] toUser = InternetAddress //Destinatário(s)
+                       .parse(address);  
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject("Enviando email com JavaMail");//Assunto
+            message.setText("Enviei este email utilizando JavaMail com minha conta GMail!");
+            /**Método para enviar a mensagem criada*/
+            Transport.send(message);
+            System.out.println("Feito!!!");
+       } catch (MessagingException e) {
+            return false;
+      }
 		
-		return false;
+		return true;
 	}
 	
 	public boolean addProcesso(Processo proc) {
